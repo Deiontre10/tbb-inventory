7e37afff60af3084ff8b5c265e825e95e6eb1ec3
Taz Lowry <travontazlowry@gmail.com>
2023-04-16 12:37:21 -0400

    updated signup form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -32,52 +32,56 @@ function Signup(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+      <Link className="btn btn-link" to="/login">← Go to Login</Link>
 
       <h2>Signup</h2>
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
+        <div className="form-group">
+          <label htmlFor="firstName" class="form-label mt-4">First Name:</label>
           <input
             placeholder="First"
             name="firstName"
             type="firstName"
             id="firstName"
             onChange={handleChange}
+            className="form-control"
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
+          <label htmlFor="lastName" class="form-label mt-4">Last Name:</label>
           <input
             placeholder="Last"
             name="lastName"
             type="lastName"
             id="lastName"
             onChange={handleChange}
+            className="form-control"
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email" class="form-label mt-4">Email:</label>
           <input
             placeholder="youremail@test.com"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
+            className="form-control"
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+          <label htmlFor="pwd" class="form-label mt-4">Password:</label>
           <input
             placeholder="******"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
+            className="form-control"
           />
         </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+        <div>
+          <button className="btn btn-primary mt-4" type="submit">Submit</button>
         </div>
       </form>
     </div>
